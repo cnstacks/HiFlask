fd99584ac234aa64a3e838618dd5a364f6d2d447
tqtl911@163.com <19930911cXS>
2018-09-18 22:20:07 +0800

    Flask请求和响应

---
+++ 8.添加装饰器.py
@@ -0,0 +1,36 @@
+# -*- coding:utf-8 -*-
+# Project: Day123 
+# Software: PyCharm
+# Time    : 2018-09-18 19:14
+# File    : 8.添加装饰器.py
+# Author  : 天晴天朗
+# Email   : tqtl@tqtl.org
+from flask import Flask
+import functools
+
+app = Flask(__name__)
+
+
+def wapper(func):
+    @functools.wraps(func)
+    def inner(*args, **kwargs):
+        print('before')
+        return func(*args, **kwargs)
+
+    return inner
+
+
+@app.route('/xxxx/', methods=['GET', 'POST'])
+@wapper
+def index():
+    return 'Index'
+
+
+@app.route('/xxxx/', methods=['GET', 'POST'])
+@wapper
+def order():
+    return 'Order'
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ 9.Flask框架之CBV和FBV.py
@@ -0,0 +1,56 @@
+# -*- coding:utf-8 -*-
+# Project: Day123 
+# Software: PyCharm
+# Time    : 2018-09-18 21:57
+# File    : 9.Flask框架之CBV和FBV.py
+# Author  : 天晴天朗
+# Email   : tqtl@tqtl.org
+from flask import Flask, redirect, render_template, views
+
+app = Flask(__name__)
+import functools
+
+
+def wapper(func):
+    @functools.wraps(func)
+    def inner(*args, **kwargs):
+        print('before')
+        return func(*args, **kwargs)
+
+    return inner
+
+
+@app.route('/xxx/', methods=['GET', 'POST'])
+@wapper
+def index():
+    return 'Index'
+
+
+class IndexView(views.View):
+    methods = ['GET']
+    decorators = [wapper, ]
+
+    def dispatch_request(self):
+        print('Index')
+        return 'Index'
+
+
+app.add_url_rule('/index/', view_func=IndexView.as_view(name='index'))  # name == endpoint
+
+
+# CBV方式;
+class IndexView(views.MethodView):
+    methods = ['GET']
+    decorators = [wapper]
+
+    def get(self):
+        return 'Index.GET'
+
+    def post(self):
+        return 'Index POST'
+
+
+app.add_url_rule('/index/', view_func=IndexView.as_view(name='index'))  # name = endpoint
+
+if __name__ == '__main__':
+    app.run()
